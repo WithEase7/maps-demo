941aa08cfe94caed14698ae8f5e78be8c50df23c
withease <abhishekbhaskar171@gmail.com>
2021-12-27 20:56:18 +0530

    third commit

--- App.js
+++ App.js
@@ -8,21 +8,6 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 const Stack = createNativeStackNavigator();
 
-const Header = () => {
-  return (
-    <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '95%'}}>
-      <TextInput
-        placeholder="Enter the city"
-        style={{color: 'black', fontSize: 15}}
-        onChangeText={e=>console.log(e)}
-      />
-      <TouchableOpacity>
-        <Icon name="search-outline" size={25} />
-      </TouchableOpacity>
-    </View>
-  );
-};
-
 const App = () => {
   return (
     <NavigationContainer>
@@ -40,7 +25,6 @@ const App = () => {
           component={MapComponent}
           options={{title: "Map"}}
           // options={({ route }) => ({ title: route.params.name })}
-          // options={{headerTitle: () => <LogoTitle />}}
           initialParams={{itemId: 20}}
         />
       </Stack.Navigator>
